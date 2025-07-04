9a144623a48df99bb8aadfbbc40113fd867a0c01
Mohamed Abdulla <97242324+Mohamed-Abdulla@users.noreply.github.com>
2025-07-04 20:30:09 +0530

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,8 @@
-const getImagePrefix = () => {
-  return process.env.NODE_ENV === "production"
-    ? "/Crypgo/"
-    : "";
-};
-
+// const getImagePrefix = () => {
+//   return process.env.NODE_ENV === "production"
+//     ? "/Crypgo/"
+//     : "";
+// };
+const getImagePrefix = () => "";
 export { getImagePrefix };
  
